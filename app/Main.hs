7949f6d3b0b3f6cd34d48117211251d265dc9d5d
codygman <cody@codygman.dev>
2020-06-11 19:21:24 -0500

    add some code and docs to test ghcide

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,11 @@
 module Main where
 
-import Lib
+import qualified Lib
+import qualified Text.Pandoc as Pandoc
+import qualified Data.Map as Map
+import qualified Data.ByteString.Char8 as BS
 
 main :: IO ()
-main = someFunc
+main = do
+  print $ Pandoc.Meta Map.empty
+  print $ BS.pack "foo"
--- src/Lib.hs
+++ src/Lib.hs
@@ -2,5 +2,6 @@ module Lib
     ( someFunc
     ) where
 
+-- | prints out 'someFunc'
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
