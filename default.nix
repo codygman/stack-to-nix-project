fa1c61856e2d35741e6f58b313eebe7416f08a28
codygman <cody@codygman.dev>
2020-06-11 19:19:04 -0500

    add hoogle

--- default.nix
+++ default.nix
@@ -24,6 +24,7 @@ let
                        cabal-install
                        ghcid
                        ghcide
+                       hoogle
                        hlint
                        hpack
                      ];
